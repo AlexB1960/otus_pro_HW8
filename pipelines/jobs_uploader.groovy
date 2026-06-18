3781ae85232ebec298418a303550e470c6d0ce4e
Alexander <aburlaka@rambler.ru>
2026-06-18 18:54:57 +0300

    hw-8 first commit

---
+++ pipelines/jobs_uploader.groovy
@@ -0,0 +1,31 @@
+
+timeout(60) {  //время сборки в секундах
+   node("ansible") { //значение поля Labels из настроек в Docker Agent templates
+     currentBuild.description = "Upload jobs on Jenkins" //заголовок у каждой джобы
+
+     stage("Checkout") { //скачиваем исходники с удаленного репозитория. scm должен быть в макросах
+       checkout scm
+     }
+
+     stage("Create config file") { //создание конфиг-файла
+       //забираем креды, созданные нами в Jenkins-Credentials. они хранятся только в пределах этих(внизу) фигурных скобок
+       withCredentials([usernamePassword(credentialsId: 'jenkins', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASSWORD')]) {
+         writeFile file: './config.ini', test: """[jenkins]
+           url=${env.JENKINS_URL}
+           user=${JENKINS_USER}
+           password=${JENKINS_PASSWORD}
+
+        [job_builder]
+        recursive=True
+        keep_description=True
+        ignore_cache=True
+        update=all
+"""
+       }
+     }
+
+     stage("Upload jobs on Jenkins") { //выполняем накатку джоб в дженкинс
+        sh "jenkins-jobs --conf ./config.ini --flush-cache update ./jobs" // ./jobs - это путь, где у нас лежат джобы
+     }
+   }
+}
