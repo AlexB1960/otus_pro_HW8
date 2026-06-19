8d1a52642a7a172ee249455c38dacbda8a582076
Alexander <aburlaka@rambler.ru>
2026-06-19 21:24:21 +0300

    scm

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -1,7 +1,11 @@
 
 timeout(60) {
    node("ansible") {
-     currentBuild.description = "Upload jobs on Jenkins"
+     currentBuild.description = "Upload jobs on jenkins"
+
+     stage("Checkout") {
+       checkout scm
+     }
 
      stage("Create config file") {
        withCredentials([usernamePassword(credentialsId: 'jenkins', usernameVariable: 'JENKINS_USER', passwordVariable: 'JENKINS_PASSWORD')]) {
@@ -13,13 +17,11 @@ timeout(60) {
         [job_builder]
         recursive=True
         keep_description=True
-        ignore_cache=True
-        update=all
 """
        }
      }
 
-     stage("Upload jobs on Jenkins") {
+     stage("Upload jobs on jenkins") {
         sh "jenkins-jobs --conf ./config.ini --flush-cache update ./jobs"
      }
    }
