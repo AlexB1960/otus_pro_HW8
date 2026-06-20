20034dc4657bd51f8386c8f0f10f78e9a3dee933
Alexander <aburlaka@rambler.ru>
2026-06-21 00:20:59 +0300

    with 1gb for ui-tests

--- pipelines/ui_tests.groovy
+++ pipelines/ui_tests.groovy
@@ -4,7 +4,7 @@ timeout(500) {
      currentBuild.description = "Running ui-tests on Jenkins"
 
      stage("Running ui-tests on Jenkins") {
-        sh "docker run --rm --network host tests_ui:1.0"
+        sh "docker run -it --shm-size=1gb --rm --network host tests_ui:1.0"
      }
    }
 }
